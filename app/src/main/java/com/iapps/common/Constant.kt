59437631b9a332e6405fbd4cddd923d561804f13
Digvesh Patel <digvesh.patel@tatvasoft.com>
2024-02-28 12:39:52 +0530

    Display the data in a vertical RecyclerView list - one column on smartphones and 3 columns on larger screens,

--- app/src/main/java/com/iapps/common/Constant.kt
+++ app/src/main/java/com/iapps/common/Constant.kt
@@ -1,10 +1,14 @@
 package com.iapps.common
 
 
-// error message
+// Error Message
 const val SOMETHING_WENT_WRONG = "something went wrong please try again later"
 const val NO_INTERNET_CONNECTION = "No internet connection"
 const val HTTP_ERROR = "HTTP error"
+
+// BASE URL
 const val BASE_URL = "https://api.flickr.com"
+
+// DATABASE
 const val I_APPS_DATABASE = "IAppsDatabase"
 
